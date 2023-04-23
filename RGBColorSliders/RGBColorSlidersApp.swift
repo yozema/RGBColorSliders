62ae4fdc5f14719440d8cd770e7becfde4a0158d
Ilya Zemskov <zemskov.ilya@gmail.com>
2023-04-23 17:44:09 -0300

    Initial Commit

---
+++ RGBColorSliders/RGBColorSlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBColorSlidersApp.swift
+//  RGBColorSliders
+//
+//  Created by Ilya Zemskov on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBColorSlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
